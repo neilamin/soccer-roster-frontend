d109c1ed4275af62b278befba3949395ecbd1dbd
Neil Amin <namin@tsys.com>
2019-11-20 10:41:46 -0500

    Init connection to API

--- src/views/Dashboard/DashboardView.jsx
+++ src/views/Dashboard/DashboardView.jsx
@@ -1,9 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-export default () => (
+export default () => {
+  const [teams, updateTeams] = useState([])
+
+  console.log(teams)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://localhost:5000/teams'
+      )
+      updateTeams(result.data)
+    }
+
+    fetchData()
+  }, [])
+
+  return (
     <div>
-        <h1>
-            Welcome to My Soccer Roster App!
-        </h1>
+      <h1>Welcome to My Soccer Roster App!</h1>
     </div>
-)
+  )
+}
